2515c719d13bafc062b3e75ef66c997fef1fd2f4
henryhschen <tool830@gmail.com>
2017-07-17 23:27:23 +0800

    2nd check-in

--- python2.py
+++ python2.py
@@ -6,10 +6,16 @@
 import sys;
 
 # 1
-x = 'foo';
-sys.stdout.write(x + '\n');
+#x = 'foo';
+#sys.stdout.write(x + '\n');
 
 # 2
-print('# ', len(sys.argv));
-print('Arg: ', str(sys.argv));
+#print('# ', len(sys.argv));
+#print('Arg: ', str(sys.argv));
 
+
+import numpy as np
+filename = "text"
+raw_data = open(filename, "rt")
+dataset = np.loadtxt(raw_data, delimiter=",")
+print(dataset)
